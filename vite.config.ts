a7c306988ccd0ecc931867db7ac5537cd3908cc5
Teodor <burov96@gmail.com>
2024-11-25 13:33:40 +0200

    fix: resolve merge conflicts in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: '/',
-=======
-  base: './',
->>>>>>> 36b7e007d5874d15aca42efb917b23cd343e59ac
+  base: '/login/',
 })
